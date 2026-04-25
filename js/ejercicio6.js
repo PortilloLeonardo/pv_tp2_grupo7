853c0179585c9ded3384bbbe22fea5879ce64019
milagrosgit7-source <milagrosgit7@gmail.com>
2026-04-25 18:14:41 -0300

    Refactor comments and initialize edades array
    Updated comments for clarity and corrected array declaration.

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,10 +1,18 @@
-/*Ejercicio 6: crear un array con al menos 8 edades distintas.
-Recorrer el array,mostrar las edades y el promedio de las edades.*/
+// Ejercio 6 - TP2
+// Consigna: Crear un array con al menos 8 edades distintas
+// Recorrer el array, mostrar las edades y calcular el promedio
+
+// Array ccon 8 edades distintas
 const edades = [18, 20, 22, 19, 25, 30, 21, 23];
-let suma = 0;
+let suma = 0; // Acumular para sumar todas las edades
+
+// forEach recorrera cada elemento del array
+// Por cada edad: la muestra por consola y la suma al acumulador 
 edades.forEach((edad) => {
     console.log(`Edad: ${edad}`);
     suma = suma + edad;
 });
+
+//Se divide la suma total por la cantidad de elementos del array
 let promedio = suma / edades.length;
 console.log(`Promedio de edades: ${promedio}`);
