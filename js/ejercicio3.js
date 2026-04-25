71e962ac74a118d1528a5c7feb2d1556d5a7bad3
milagrosgit7-source <milagrosgit7@gmail.com>
2026-04-25 17:24:48 -0300

    Update ejercicio3.js

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,8 +1,12 @@
 // Ejercicio 3 - TP2
 // Consigna: Calcular y mostrar por consola el volumen de una esfera cuyo radio = 4 m.
 
+//Funcion flecha que recibe el radio y aplica la formula v = (4/3) * π * r³
 const calcularVolumen = (radio) => {
+  //Math.PI provee el valor de π
+  //Math.pow (radio, 3) eleva el radio a la potencia 3
   console.log((4 / 3) * Math.PI * Math.pow(radio, 3));
 };
 
+//Se llama a la funcion con radio = 4
 calcularVolumen(4);
