5a17e5dd84f88330bc68c1d5da3492d7d709410a
PortilloLeonardo <leoydanixd@gmail.com>
2026-05-03 21:23:19 -0300

    Resolucion ejercicio 25

---
+++ js/funciones25.js
@@ -0,0 +1,10 @@
+//Generamos una cadena de texto que concatena una composicion hexagesimal del colores.
+
+export const generarColorAleatorio = () => {
+  const letras = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letras[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
